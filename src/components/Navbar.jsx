d5a3408521ce06f77c899c26817a47a60f4f8e9a
Alejandro Cuartas <cuartasmarin734@gmail.com>
2021-09-16 12:12:26 -0500

    last update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,17 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/"><h1>Home</h1></Link>
+        <Link className="navbar-brand" to="/Math-tools-app/"><h1>Home</h1></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/calculator">Calculator</Link>
+              <Link className="nav-link active" aria-current="page" to="/Math-tools-app/calculator">Calculator</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/facts">Number facts</Link>
+              <Link className="nav-link active" aria-current="page" to="/Math-tools-app/facts">Number facts</Link>
             </li>
           </ul>
         </div>
